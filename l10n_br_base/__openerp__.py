8dc9c389f3f650e291f3c7cc97ce42eab759c609
Leonardo Santagada <santagada@gmail.com>
2011-11-19 20:14:32 -0200

    l10n_br_base refatored

--- l10n_br_base/__openerp__.py
+++ l10n_br_base/__openerp__.py
@@ -1,21 +1,4 @@
 # -*- encoding: utf-8 -*-
-###############################################################################
-#                                                                             #
-# Copyright (C) 2009  Renato Lima - Akretion                                  #
-#                                                                             #
-#This program is free software: you can redistribute it and/or modify         #
-#it under the terms of the GNU General Public License as published by         #
-#the Free Software Foundation, either version 3 of the License, or            #
-#(at your option) any later version.                                          #
-#                                                                             #
-#This program is distributed in the hope that it will be useful,              #
-#but WITHOUT ANY WARRANTY; without even the implied warranty of               #
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
-#GNU General Public License for more details.                                 #
-#                                                                             #
-#You should have received a copy of the GNU General Public License            #
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
-###############################################################################
 
 {
     'name': 'Brazilian Localization Base',
@@ -31,7 +14,7 @@
         'l10n_br_base.city.csv',
         ],
     'update_xml': [
-	'l10n_br_base_data.xml',
+    'l10n_br_base_data.xml',
         'l10n_br_base_view.xml',
         'country_view.xml',
         'partner_view.xml',
@@ -41,5 +24,3 @@
     'demo_xml': ['l10n_br_base_demo.xml'],
     'installable': True
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- l10n_br_base/l10n_br_base.py
+++ l10n_br_base/l10n_br_base.py
@@ -1,27 +1,8 @@
 # -*- encoding: utf-8 -*-
-#################################################################################
-#                                                                               #
-# Copyright (C) 2009  Renato Lima - Akretion                                    #
-#                                                                               #
-#This program is free software: you can redistribute it and/or modify           #
-#it under the terms of the GNU General Public License as published by           #
-#the Free Software Foundation, either version 3 of the License, or              #
-#(at your option) any later version.                                            #
-#                                                                               #
-#This program is distributed in the hope that it will be useful,                #
-#but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
-#GNU General Public License for more details.                                   #
-#                                                                               #
-#You should have received a copy of the GNU General Public License              #
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
-#################################################################################
 
 from osv import osv, fields
 
-#################################################################################
-# Municipios e Códigos do IBGE
-#################################################################################
+
 class l10n_br_base_city(osv.osv):
     _name = 'l10n_br_base.city'
     _description = 'Municipios e Códigos do IBGE'
@@ -29,12 +10,11 @@ class l10n_br_base_city(osv.osv):
         'name': fields.char('Nome', size=64, required=True),
         'state_id': fields.many2one('res.country.state', 'Estado', required=True),
         'ibge_code': fields.char('Codigo IBGE', size=7),
-    }
+        }
+
 l10n_br_base_city()
 
-#################################################################################
-# CEP - Código de endereçamento Postal
-#################################################################################
+
 class l10n_br_base_cep(osv.osv):
     _name = 'l10n_br_base.cep'
     _rec_name = 'code'
@@ -46,6 +26,5 @@ class l10n_br_base_cep(osv.osv):
         'state_id': fields.many2one('res.country.state', 'Estado', required=True),
         'l10n_br_city_id': fields.many2one('l10n_br_base.city', 'Cidade', required=True, domain="[('state_id','=',state_id)]"),
     }
-l10n_br_base_cep()
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+l10n_br_base_cep()
