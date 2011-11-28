92767920b749b375b4ef980bd377dc6050f997fe
Leonardo Santagada <santagada@gmail.com>
2011-11-28 15:15:48 -0200

    boletos clean up

--- boleto/wizard/boleto_create.py
+++ boleto/wizard/boleto_create.py
@@ -1,22 +1,4 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#                                                                            #
-#  Copyright (C) 2011 Proge Informática Ltda (<http://www.proge.com.br>).    #
-#                                                                            #
-#  This program is free software: you can redistribute it and/or modify      #
-#  it under the terms of the GNU Affero General Public License as            #
-#  published by the Free Software Foundation, either version 3 of the        #
-#  License, or (at your option) any later version.                           #
-#                                                                            #
-#  This program is distributed in the hope that it will be useful,           #
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of            #
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
-#  GNU Affero General Public License for more details.                       #
-#                                                                            #
-#  You should have received a copy of the GNU Affero General Public License  #
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
-#                                                                            #
-##############################################################################
 
 from osv import osv, fields
 
@@ -27,7 +9,7 @@ class boleto_create(osv.osv_memory):
     _columns = {
         'success': fields.char(u'Sucesso', size=20),
         'error': fields.char(u'Erros', size=20),
-    }
+        }
 
     def create(self, cr, uid, ids, context=None):
         inv_obj = self.pool.get('account.invoice').browse(cr, uid, ids)
